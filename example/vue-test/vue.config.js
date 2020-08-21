1709c6c69f67fcaf6e5188c5e6c0b0bf8a29830e
changge <changgeee@163.com>
2020-08-21 14:10:45 +0800

    update readme (#8)
    * add image
    
    * add image
    
    * readme images
    
    * add codepen url
    
    * readme
    
    * flex
    
    * example
    
    * add example
    
    * update readme

--- example/vue-test/vue.config.js
+++ example/vue-test/vue.config.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+=======
+>>>>>>> 4e718c1930c03b29baa8d584b6c773489247552f
 module.exports = {
   css: {
     loaderOptions: {
@@ -7,5 +14,19 @@ module.exports = {
         }
       }
     }
+<<<<<<< HEAD
+  },
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ])
+      })
+    ]
+=======
+>>>>>>> 4e718c1930c03b29baa8d584b6c773489247552f
   }
 };
